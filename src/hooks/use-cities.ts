4fc3ac51912e002fe3eed789224fb3553eed0e68
Kevin GALLET <kevingallet@gmail.com>
2025-01-21 17:17:49 +0100

    feat: add popular cities

--- src/hooks/use-cities.ts
+++ src/hooks/use-cities.ts
@@ -2,8 +2,16 @@ import { useQuery } from '@tanstack/react-query'
 import { useQueryState } from 'nuqs'
 import { TCity } from '~/schemas/territories'
 
-export const fetchCitiesByDepartmentCode = async (code: string): Promise<TCity[]> => {
-  const response = await fetch(`/api/territories/cities?department=${code}`)
+export const fetchCitiesByDepartmentCode = async (code: string | null): Promise<TCity[]> => {
+  const baseUrl = '/api/territories/cities'
+  const searchParams = new URLSearchParams()
+  if (!code) {
+    searchParams.set('popular', 'true')
+  } else {
+    searchParams.set('department', code)
+  }
+
+  const response = await fetch(`${baseUrl}?${searchParams.toString()}`)
   if (!response.ok) {
     throw new Error('Error occurred calling API retrieving territories')
   }
@@ -14,8 +22,7 @@ export const useCities = () => {
   const [departmentCode] = useQueryState('department')
 
   const { data, isError, isLoading } = useQuery<TCity[]>({
-    enabled: !!departmentCode,
-    queryFn: () => fetchCitiesByDepartmentCode(departmentCode as string),
+    queryFn: () => fetchCitiesByDepartmentCode(departmentCode),
     queryKey: ['cities-by-department', departmentCode],
   })
   return {
