578a4ce0622292f324a8847b5fe6c8a648490e83
Kevin GALLET <kevingallet@gmail.com>
2025-01-27 16:23:10 +0100

    fix: build and change alert accommodation image src

--- src/app/alerte-logement/page.tsx
+++ src/app/alerte-logement/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import styles from './alert-logement.module.css'
-import findNextAccommodation from '~/images/find-next-accommodation.svg'
+import findNextAccommodation from '~/images/find-next-accommodation.webp'
 import Input from '@codegouvfr/react-dsfr/Input'
 import Button from '@codegouvfr/react-dsfr/Button'
 import { getTranslations } from 'next-intl/server'
@@ -9,7 +9,7 @@ export default async function AlerteLogement() {
   const t = await getTranslations('alertLogement')
   return (
     <div className={styles.container}>
-      <Image className={styles.image} src={findNextAccommodation.src} alt="Restez alerté des dernières annonces" width={588} height={459} />
+      <Image src={findNextAccommodation} alt="Restez alerté des dernières annonces" priority quality={100} />
       <div className={styles.cardContainer}>
         <h1 className={styles.title}>{t('title')}</h1>
         <p>{t('subTitle')}</p>
