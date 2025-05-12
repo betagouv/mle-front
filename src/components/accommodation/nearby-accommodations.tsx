c7f38f59a0422663ce710ab1badcb1345d63daba
Kevin GALLET <kevingallet@gmail.com>
2025-05-12 11:52:31 +0200

    feat: display nearby accomodation image + price

--- src/components/accommodation/nearby-accommodations.tsx
+++ src/components/accommodation/nearby-accommodations.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { fr } from '@codegouvfr/react-dsfr'
+import Badge from '@codegouvfr/react-dsfr/Badge'
 import Button from '@codegouvfr/react-dsfr/Button'
+import Card, { CardProps } from '@codegouvfr/react-dsfr/Card'
 import { useTranslations } from 'next-intl'
 import { useState } from 'react'
 import { tss } from 'tss-react'
@@ -13,10 +15,33 @@ export const NearbyAccommodations = ({ nearbyAccommodations }: { nearbyAccommoda
   const [currentIndex, setCurrentIndex] = useState(0)
   const nearbyFeatures = nearbyAccommodations.results.features
   const maxIndex = nearbyFeatures.length - 1
-
   const handlePrevious = () => setCurrentIndex((prev) => (prev > 0 ? prev - 1 : maxIndex))
-
   const handleNext = () => setCurrentIndex((prev) => (prev < maxIndex ? prev + 1 : 0))
+
+  const currentFeature = nearbyFeatures[currentIndex]?.properties
+  const hasImage = !!currentFeature?.images_urls?.[0]
+  const nbTotalApartments = currentFeature?.nb_total_apartments
+
+  const cardProps = {
+    background: true,
+    border: true,
+    badge: currentFeature.price_min ? <Badge severity="new" noIcon>{`${currentFeature.price_min}€`}</Badge> : undefined,
+    desc: <>{nbTotalApartments && <span className={fr.cx('ri-community-line')}>{`${nbTotalApartments} logements`}</span>}</>,
+    enlargeLink: true,
+    horizontal: true,
+    ...(hasImage
+      ? {
+          imageAlt: `Image de la résidence ${currentFeature?.name}`,
+          imageUrl: currentFeature?.images_urls?.[0] ?? '',
+        }
+      : {}),
+    linkProps: {
+      href: `/logement/${currentFeature?.slug}`,
+    },
+    size: 'small' as const,
+    title: currentFeature?.name,
+    titleAs: 'h5' as const,
+  } as CardProps & (typeof hasImage extends true ? CardProps.WithImageLink : CardProps.WithoutImage)
 
   return (
     <>
@@ -33,18 +58,7 @@ export const NearbyAccommodations = ({ nearbyAccommodations }: { nearbyAccommoda
           <Button size="small" onClick={handleNext} iconId="ri-arrow-right-s-line" priority="tertiary" title={t('nearby.buttons.next')} />
         </div>
       </div>
-      <div className={classes.nearbyCard}>
-        {nearbyAccommodations.results.features.length > 0 && (
-          <div key={nearbyAccommodations.results.features[currentIndex].properties.slug}>
-            <h5 className={classes.nearbyCardTitle}>{nearbyAccommodations.results.features[currentIndex].properties.name}</h5>
-            <div className={classes.nearbyCardInfo}>
-              <span className={fr.cx('ri-community-line')}>
-                {t('nearby.count', { count: nearbyAccommodations.results.features[currentIndex].properties.nb_total_apartments })}
-              </span>
-            </div>
-          </div>
-        )}
-      </div>
+      <div>{nearbyFeatures.length > 0 && <Card {...cardProps} />}</div>
     </>
   )
 }
@@ -53,25 +67,6 @@ const useStyles = tss.create({
   buttonGroup: {
     display: 'flex',
     gap: '1rem',
-  },
-  nearbyCard: {
-    background: 'white',
-    border: '1px solid var(--border-default-grey)',
-    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '1rem',
-    padding: '2rem 1rem',
-  },
-  nearbyCardInfo: {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '0.5rem',
-  },
-
-  nearbyCardTitle: {
-    color: 'var(--text-active-blue-france)',
-    cursor: 'pointer',
   },
   nearbySection: {
     alignItems: 'center',
