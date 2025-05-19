e58a223fbd14cb00001642a3c5555c3dc32243bd
Kevin GALLET <kevingallet@gmail.com>
2025-05-19 16:00:56 +0200

    feat: faq contents

--- src/app/(utils-pages)/faq/page.tsx
+++ src/app/(utils-pages)/faq/page.tsx
@@ -1,10 +1,248 @@
 import { fr } from '@codegouvfr/react-dsfr'
 import Accordion from '@codegouvfr/react-dsfr/Accordion'
-import { Tile } from '@codegouvfr/react-dsfr/Tile'
 import clsx from 'clsx'
 import { getTranslations } from 'next-intl/server'
 import { DynamicBreadcrumb } from '~/components/ui/breadcrumb'
 import styles from './faq.module.css'
+
+const contents = [
+  {
+    question: 'Quels types de logements sont accessibles aux étudiants ?',
+    answer: (
+      <>
+        <p>Plusieurs options s'offrent à vous :</p>
+        <ul>
+          <li>
+            <p className={styles.noMargin}>
+              <span className={styles.bold}>Résidences universitaires conventionnées ou à vocation sociale</span>&nbsp;: réservées aux:
+              réservées aux étudiants, elles proposent des loyers encadrés, souvent inférieurs aux prix du marché. L’accès est priorisé pour
+              les étudiants aux revenus modestes (ex. : boursiers du Crous).
+            </p>
+            <p className={styles.italic}>Inclut : résidences Crous, logements gérés par des associations ou bailleurs sociaux.</p>
+          </li>
+          <li>
+            <p>
+              <span className={styles.bold}>Résidences services étudiantes</span>&nbsp;: également réservées aux étudiants, mais avec des
+              loyers non encadrés.
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className={styles.bold}>Location classique</span>&nbsp;: logement indépendant loué auprès d’un particulier ou via une
+              agence.
+            </p>
+          </li>
+          <li>
+            <p className={styles.noMargin}>
+              <span className={styles.bold}>Logement chez l’habitant ou intergénérationnel</span>&nbsp;: chambre louée dans un logement
+              occupé, souvent avec des loyers réduits.
+            </p>
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: 'Comment comprendre les typologies de logement (T1, T2, studio, etc.) ?',
+    answer: (
+      <>
+        <ul>
+          <li>
+            <p>
+              <span className={styles.bold}>Studio</span>&nbsp;: une seule pièce à vivre avec une pièce d’eau (salle de bain/WC).
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className={styles.bold}>T1</span>&nbsp;: une pièce à vivre + une cuisine séparée + salle de bain/WC.
+            </p>
+          </li>
+          <li>
+            <p className={styles.noMargin}>
+              <span className={styles.bold}>T2, T3...</span>&nbsp;: chaque chiffre supplémentaire correspond à une pièce en plus (ex. : un
+              T2 comprend un salon et une chambre).
+            </p>
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: 'Que signifie “loyer charges comprises” ?',
+    answer: (
+      <>
+        <p>Si une annonce indique "cc" (charges comprises), cela signifie que certaines charges sont incluses dans le loyer, comme :</p>
+        <ul>
+          <li>
+            <p>L'entretien des parties communes </p>
+          </li>
+          <li>
+            <p>L'eau froide/chaude, voir l'électricité</p>
+          </li>
+        </ul>
+        <p className={clsx(styles.italic, styles.noMargin)}>⚠️ Vérifiez toujours précisément ce que couvrent les charges avant de signer.</p>
+      </>
+    ),
+  },
+  {
+    question: 'Quelle est la différence entre un logement meublé et non meublé ?',
+    answer: (
+      <>
+        <ul>
+          <li>
+            <p>
+              <span className={styles.bold}>Meublé</span>&nbsp;: contient un équipement minimum (lit, plaques de cuisson, frigo, vaisselle,
+              etc.). Les loyers sont généralement plus élevés.
+            </p>
+          </li>
+          <li>
+            <p className={styles.noMargin}>
+              <span className={styles.bold}>Non meublé</span>&nbsp;: vide ou partiellement équipé, avec un bail souvent plus long (3 ans
+              contre 1 an pour un meublé).
+            </p>
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: 'Qu’est-ce que le DPE et le GES ? Pourquoi est-ce important ?',
+    answer: (
+      <>
+        <ul>
+          <li>
+            <p>
+              <span className={styles.bold}>DPE (Diagnostic de performance énergétique)</span>&nbsp;: indique la consommation énergétique du
+              logement (note de A à G).
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className={styles.bold}>GES (Gaz à effet de serre) </span>&nbsp;: mesure les émissions liées à l’énergie utilisée.
+            </p>
+          </li>
+        </ul>
+        <p className={clsx(styles.italic, styles.noMargin)}>
+          👉 Un logement mal noté (E à G) peut être mal isolé, coûteux à chauffer et inconfortable en été.
+        </p>
+      </>
+    ),
+  },
+  {
+    question: 'Où puis-je trouver ce type de logement étudiant ?',
+    answer: (
+      <p className={styles.noMargin}>
+        Les résidences universitaires conventionnées et autres logements sociaux sont listés dans la section "Trouver un logement étudiant"
+        sur monlogementetudiant.beta.gouv.fr
+      </p>
+    ),
+  },
+  {
+    question: 'Quelles aides financières puis-je obtenir pour payer mon loyer ?',
+    answer: (
+      <>
+        <p>Plusieurs dispositifs peuvent vous aider à alléger le coût du logement :</p>
+        <ul>
+          <li>
+            <p>
+              <span className={styles.bold}>APL (Aide Personnalisée au Logement)</span>&nbsp;: versée par la CAF selon vos ressources, le
+              type de logement et le loyer.
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className={styles.bold}>ALS (Allocation de Logement Sociale)</span>&nbsp;: si vous n'êtes pas éligible à l'APL.
+            </p>
+          </li>
+          <li>
+            <p>
+              <span className={styles.bold}>FSL (Fonds de solidarité pour le logement) </span>&nbsp;: aide ponctuelle en cas de difficultés.
+            </p>
+          </li>
+          <li>
+            <p className={styles.noMargin}>
+              <span className={styles.bold}>Aides locales</span>&nbsp;: certaines régions ou villes proposent des aides spécifiques
+              (renseignez-vous auprès de votre mairie ou région).
+            </p>
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: 'Ai-je besoin d’un garant pour louer un logement ?',
+    answer: (
+      <>
+        <p>
+          Oui, la majorité des bailleurs exigent un garant : une personne (souvent un parent) qui s’engage à payer le loyer si vous ne le
+          pouvez pas. Si vous n'en avez pas, vous pouvez faire appel à :
+        </p>
+        <ul>
+          <li>
+            <p>
+              <span className={styles.bold}>La grantie Visale</span>&nbsp;(gratuite et publique)
+            </p>
+          </li>
+          <li>
+            <p className={styles.noMargin}>
+              <span className={styles.bold}>Des garanties privées payantes</span>&nbsp;: proposées par certaines plateformes de location.
+            </p>
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: 'Quels documents dois-je fournir pour constituer un dossier de location ?',
+    answer: (
+      <>
+        <p>Un dossier type comprend généralement :</p>
+        <ul>
+          <li>
+            <p>Une pièce d'identité</p>
+          </li>
+          <li>
+            <p>Un justificatif de situation étudiante (certificat de scolarité)</p>
+          </li>
+          <li>
+            <p>Les trois dernières quittances de loyer ou une attestation d’hébergement</p>
+          </li>
+          <li>
+            <p>Un justificatif de ressources (ou ceux du garant)</p>
+          </li>
+          <li>
+            <p className={styles.noMargin}>Le contrat de travail ou une attestation de bourse, si applicable</p>
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: 'Que dois-je vérifier avant de signer un bail ?',
+    answer: (
+      <>
+        <p>Avant de vous engager, pensez à vérifier :</p>
+        <ul>
+          <li>
+            <p>L'état des lieux d'entrée</p>
+          </li>
+          <li>
+            <p>Ce qui couvre exactement les charges</p>
+          </li>
+          <li>
+            <p>La durée du bail et les modalités de résiliation</p>
+          </li>
+          <li>
+            <p>La conformité du logement (surface minimale, équipements obligatoires pour un meublé, etc.)</p>
+          </li>
+          <li>
+            <p className={styles.noMargin}>L’existence d’une clause de solidarité si vous êtes en colocation</p>
+          </li>
+        </ul>
+      </>
+    ),
+  },
+]
 
 export default async function Faq() {
   const t = await getTranslations('faq')
@@ -14,61 +252,20 @@ export default async function Faq() {
       <DynamicBreadcrumb margin={false} />
       <h1 style={{ margin: 0 }}>{t('title')}</h1>
       <div className={styles.descriptionContainer}>
-        <p style={{ margin: 0 }}>{t('subTitle')}</p>
+        <p className={styles.description}>{t('subTitle')}</p>
       </div>
-      <div className={styles.tilesContainer}>
-        <Tile
-          linkProps={{
-            href: '#',
-          }}
-          orientation="vertical"
-          small
-          title="Lorem ipsum dolor consectetur"
-          titleAs="h3"
-        />
-        <Tile
-          linkProps={{
-            href: '#',
-          }}
-          orientation="vertical"
-          small
-          title="Lorem ipsum dolor consectetur"
-          titleAs="h3"
-        />
-        <Tile
-          enlargeLinkOrButton
-          linkProps={{
-            href: '#',
-          }}
-          orientation="vertical"
-          small
-          title="Lorem ipsum dolor consectetur"
-          titleAs="h3"
-        />
-        <Tile
-          linkProps={{
-            href: '#',
-          }}
-          orientation="vertical"
-          small
-          title="Lorem ipsum dolor consectetur"
-          titleAs="h3"
-        />
-      </div>
-      <div className={fr.cx('fr-py-3w')}>
-        <h2>Lorem ipsum dolor consectetur</h2>
+      <div className={fr.cx('fr-pb-3w')}>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.title}>{t('popularQuestions')}</h2>
+          <hr className={styles.border} />
+        </div>
         <div className={styles.accordionContainer}>
           <div className={fr.cx('fr-accordions-group')}>
-            <Accordion label="Proident aliquip nisi aliqua irure fugiat elit commodo dolore in veniam.">
-              Incididunt pariatur consectetur cillum nostrud esse in est ex aute. Non proident incididunt nulla eu incididunt ipsum dolore
-              aliqua tempor incididunt culpa. Ad nisi excepteur do ea adipisicing. Velit enim esse irure adipisicing esse. Cupidatat culpa
-              eu qui anim.
-            </Accordion>
-            <Accordion label="Proident aliquip nisi aliqua irure fugiat elit commodo dolore in veniam.">
-              Incididunt pariatur consectetur cillum nostrud esse in est ex aute. Non proident incididunt nulla eu incididunt ipsum dolore
-              aliqua tempor incididunt culpa. Ad nisi excepteur do ea adipisicing. Velit enim esse irure adipisicing esse. Cupidatat culpa
-              eu qui anim.
-            </Accordion>
+            {contents.map((content, index) => (
+              <Accordion key={index} label={content.question}>
+                {content.answer}
+              </Accordion>
+            ))}
           </div>
         </div>
       </div>
