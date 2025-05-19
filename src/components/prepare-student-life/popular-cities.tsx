3c722c6831ceaafd36d96bb37b25ab8360d6db1a
Kevin GALLET <kevingallet@gmail.com>
2025-05-19 12:02:08 +0200

    fix: do not display popular cities title when department set + null values

--- src/components/prepare-student-life/popular-cities.tsx
+++ src/components/prepare-student-life/popular-cities.tsx
@@ -2,10 +2,13 @@
 
 import Alert from '@codegouvfr/react-dsfr/Alert'
 import Tile from '@codegouvfr/react-dsfr/Tile'
+import { useTranslations } from 'next-intl'
+import { useQueryState } from 'nuqs'
 import { FC } from 'react'
 import { tss } from 'tss-react'
 import { useCities } from '~/hooks/use-cities'
 import { TCity } from '~/schemas/territories'
+import { sPluriel } from '~/utils/sPluriel'
 
 interface PopularCitiesProps {
   cities: TCity[]
@@ -13,6 +16,9 @@ interface PopularCitiesProps {
 
 export const PopularCities: FC<PopularCitiesProps> = ({ cities }) => {
   const { classes } = useStyles()
+  const t = useTranslations('prepareStudentLife')
+  const [departmentQuery] = useQueryState('department')
+
   const { data, isLoading } = useCities()
 
   if (isLoading) return null
@@ -25,25 +31,34 @@ export const PopularCities: FC<PopularCitiesProps> = ({ cities }) => {
         description="Il n'y a pas de logements pour les villes de ce département. Nous vous invitons à réessayer plus tard, de nouvelles résidences sont ajoutées régulièrement."
       />
     )
-
   return (
-    <div className={classes.tilesGrid}>
-      {(data || cities).map((city) => (
-        <Tile
-          noIcon
-          key={city.id}
-          desc={`${city.nb_total_apartments} logements étudiants`}
-          detail={city.price_min ? `Budget minimum ${city.price_min}€` : undefined}
-          linkProps={{
-            href: `/preparer-sa-vie-etudiante/${city.slug}`,
-          }}
-          orientation="vertical"
-          title={city.name}
-          titleAs="h3"
-          classes={{ root: classes.tileContainer }}
-        />
-      ))}
-    </div>
+    <>
+      {!departmentQuery && (
+        <div className={classes.popularCitiesTitle}>
+          <h3>{t('popularCities')}</h3>
+        </div>
+      )}
+      <div className={classes.tilesGrid}>
+        {(data || cities).map((city) => {
+          const desc = !!city.nb_total_apartments && `${city.nb_total_apartments} logement${sPluriel(city.nb_total_apartments)} étudiants`
+          return (
+            <Tile
+              noIcon
+              key={city.id}
+              desc={desc}
+              detail={city.price_min ? `Budget minimum ${city.price_min}€` : undefined}
+              linkProps={{
+                href: `/preparer-sa-vie-etudiante/${city.slug}`,
+              }}
+              orientation="vertical"
+              title={city.name}
+              titleAs="h3"
+              classes={{ root: classes.tileContainer }}
+            />
+          )
+        })}
+      </div>
+    </>
   )
 }
 const useStyles = tss.create({
@@ -57,5 +72,11 @@ const useStyles = tss.create({
   },
   tileContainer: {
     height: '160px',
+  },
+  popularCitiesTitle: {
+    color: '#666666',
+    display: 'flex',
+    justifyContent: 'center',
+    marginBottom: '1rem',
   },
 })
