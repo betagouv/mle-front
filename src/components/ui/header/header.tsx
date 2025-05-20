36680e27ab139f204c34e800450f4bdbe58099cf
Kevin GALLET <kevingallet@gmail.com>
2025-05-20 10:09:32 +0200

    feat: add banner

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -4,6 +4,7 @@ import { Header } from '@codegouvfr/react-dsfr/Header'
 import { getTranslations } from 'next-intl/server'
 import { FC } from 'react'
 import { BrandTop } from '~/components/ui/brand-top'
+import { Banner } from '~/components/ui/header/banner/banner'
 import { HeaderNavigation } from '~/components/ui/header/navigation'
 import logo from '~/images/logo.svg'
 import { getAcademies } from '~/server-only/get-academies'
@@ -13,37 +14,40 @@ export const HeaderComponent: FC = async () => {
   const academies = await getAcademies()
 
   return (
-    <Header
-      homeLinkProps={{
-        href: '/',
-        title: t('metadata.homeLinkTitle'),
-      }}
-      quickAccessItems={[
-        <Button key="faq-cta" priority="tertiary no outline" iconId="ri-question-line" linkProps={{ href: '/faq', target: '_self' }}>
-          {t('navigation.faq')}
-        </Button>,
-        <Button
-          priority="tertiary no outline"
-          key="alerts-cta"
-          iconId="ri-notification-3-line"
-          linkProps={{ href: '/alerte-logement', target: '_self' }}
-        >
-          {t('header.alerts')}
-        </Button>,
-        <Button priority="tertiary" key="login-cta" iconId="ri-user-line" linkProps={{ href: '/se-connecter', target: '_self' }}>
-          {t('header.login')}
-        </Button>,
-      ]}
-      brandTop={<BrandTop />}
-      serviceTagline={t('header.description')}
-      serviceTitle={t('header.title')}
-      navigation={<HeaderNavigation academies={academies} />}
-      className={fr.cx('fr-header')}
-      operatorLogo={{
-        alt: 'Mon logement étudiant - logo',
-        imgUrl: logo.src,
-        orientation: 'horizontal',
-      }}
-    />
+    <div>
+      <Header
+        homeLinkProps={{
+          href: '/',
+          title: t('metadata.homeLinkTitle'),
+        }}
+        quickAccessItems={[
+          <Button key="faq-cta" priority="tertiary no outline" iconId="ri-question-line" linkProps={{ href: '/faq', target: '_self' }}>
+            {t('navigation.faq')}
+          </Button>,
+          <Button
+            priority="tertiary no outline"
+            key="alerts-cta"
+            iconId="ri-notification-3-line"
+            linkProps={{ href: '/alerte-logement', target: '_self' }}
+          >
+            {t('header.alerts')}
+          </Button>,
+          <Button priority="tertiary" key="login-cta" iconId="ri-user-line" linkProps={{ href: '/se-connecter', target: '_self' }}>
+            {t('header.login')}
+          </Button>,
+        ]}
+        brandTop={<BrandTop />}
+        serviceTagline={t('header.description')}
+        serviceTitle={t('header.title')}
+        navigation={<HeaderNavigation academies={academies} />}
+        className={fr.cx('fr-header')}
+        operatorLogo={{
+          alt: 'Mon logement étudiant - logo',
+          imgUrl: logo.src,
+          orientation: 'horizontal',
+        }}
+      />
+      <Banner />
+    </div>
   )
 }
