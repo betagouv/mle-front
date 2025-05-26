51c2383387646b8697365dd13f8e326a5303e7d6
Kevin GALLET <kevingallet@gmail.com>
2025-05-26 16:23:45 +0200

    feat: placeholder images

--- src/components/find-student-accomodation/card/find-student-accommodation-image-card.tsx
+++ src/components/find-student-accomodation/card/find-student-accommodation-image-card.tsx
@@ -1,7 +1,31 @@
 import Image from 'next/image'
 import { parseAsString, useQueryState } from 'nuqs'
 
+const PLACEHOLDER_IMAGES = [
+  '/images/placeholder/logement-etudiant-chambre-1.jpg',
+  '/images/placeholder/logement-etudiant-chambre-2.jpg',
+  '/images/placeholder/logement-etudiant-chambre-3.jpg',
+  '/images/placeholder/logement-etudiant-chambre-4.jpg',
+  '/images/placeholder/logement-etudiant-chambre-5.jpg',
+]
+
+export const FindStudentAccommodationPlaceholderImageCard = () => {
+  const [vue] = useQueryState('vue', parseAsString)
+  const randomImage = PLACEHOLDER_IMAGES[Math.floor(Math.random() * PLACEHOLDER_IMAGES.length)]
+
+  return (
+    <Image
+      style={{ objectFit: 'cover' }}
+      src={randomImage}
+      alt="Image placeholder d'une résidence"
+      width={vue === 'carte' ? 332 : 390}
+      height={193}
+      priority
+    />
+  )
+}
+
 export const FindStudentAccommodationImageCard = ({ image, name }: { image: string; name: string }) => {
   const [vue] = useQueryState('vue', parseAsString)
-  return <Image style={{ objectFit: 'cover' }} src={image} alt={name} width={vue === 'carte' ? 344 : 376} height={193} priority />
+  return <Image style={{ objectFit: 'cover' }} src={image} alt={name} width={vue === 'carte' ? 344 : 390} height={193} priority />
 }
