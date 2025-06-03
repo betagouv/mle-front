31085330c9a722e239a71dbd476381c3ccf0ebb6
Kevin GALLET <kevingallet@gmail.com>
2025-06-03 14:41:59 +0200

    chore: fix padding on owner details

--- src/components/find-student-accomodation/owner-details/owner-details.tsx
+++ src/components/find-student-accomodation/owner-details/owner-details.tsx
@@ -19,29 +19,29 @@ export const OwnerDetails = async ({ nbTotalApartments, owner, externalUrl, titl
   const ownerUrl = externalUrl || owner?.url
   return (
     <div className={styles.sidebarCard}>
-      {nbTotalApartments ? (
-        <h3 className={styles.sidebarTitle}>{t('sidebar.accommodationsCount', { count: nbTotalApartments })}</h3>
-      ) : (
-        <h3 className={styles.sidebarTitle}>{t('sidebar.accommodationsNoCount')}</h3>
-      )}
-      {!!owner && (
-        <div className={styles.sidebarOwner}>
-          <span>{t('sidebar.proposedBy')}</span>
-          {owner.image_base64 ? (
-            <Image src={owner.image_base64} alt={owner.name} width={201} height={90} quality={100} />
-          ) : (
-            <h3 className={fr.cx('fr-m-0')}>{owner.name}</h3>
-          )}
-          {!!ownerUrl && (
-            <>
-              <span className={fr.cx('fr-text--sm', 'fr-m-0')}>{t('sidebar.hasAvailableAccommodation')}</span>
-              <Button linkProps={{ href: ownerUrl }} priority="primary" size="large">
-                {t('sidebar.buttons.consult')}
-              </Button>
-            </>
-          )}
-        </div>
-      )}
+      <div className={styles.sidebarHeader}>
+        {nbTotalApartments ? (
+          <h3 className={styles.sidebarTitle}>{t('sidebar.accommodationsCount', { count: nbTotalApartments })}</h3>
+        ) : (
+          <h3 className={styles.sidebarTitle}>{t('sidebar.accommodationsNoCount')}</h3>
+        )}
+        <span>{t('sidebar.proposedBy')}</span>
+        {owner?.image_base64 ? (
+          <Image src={owner.image_base64} alt={owner.name} width={201} height={90} quality={100} />
+        ) : (
+          <h3 className={fr.cx('fr-m-0')}>{owner?.name}</h3>
+        )}
+      </div>
+      <div className={styles.sidebarOwner}>
+        {!!ownerUrl && (
+          <>
+            <span className={fr.cx('fr-text--sm', 'fr-m-0')}>{t('sidebar.hasAvailableAccommodation')}</span>
+            <Button linkProps={{ href: ownerUrl }} priority="primary" size="large" className={styles.sidebarOwnerButton}>
+              {t('sidebar.buttons.consult')}
+            </Button>
+          </>
+        )}
+      </div>
       <OwnerDetailsActions title={title} location={location} />
     </div>
   )
