32f3155c761276d55ce3d2b2de27d2083d2c7857
Kevin GALLET <kevingallet@gmail.com>
2025-06-03 09:48:37 +0200

    feat: add residences in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,19 +1,35 @@
 import type { MetadataRoute } from 'next'
 import { z } from 'zod'
+import { getAccommodations } from '~/server-only/get-accommodations'
 import { getPopularCities } from '~/server-only/get-popular-cities'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const popularCities = await getPopularCities()
   const baseUrl = z.string().parse(process.env.BASE_URL)
   const lastModified = new Date()
-  const prepareStudentLifeMap: MetadataRoute.Sitemap = popularCities.map((city) => ({
+
+  const prepareStudentLifeMetadata: MetadataRoute.Sitemap = popularCities.map((city) => ({
     url: `${baseUrl}/preparer-sa-vie-etudiante/${city.slug}`,
     lastModified,
     changeFrequency: 'weekly',
     priority: 0.8,
   }))
-  const findStudentAccommodationMap: MetadataRoute.Sitemap = popularCities.map((city) => ({
+  const findStudentAccommodationMetadata: MetadataRoute.Sitemap = popularCities.map((city) => ({
     url: `${baseUrl}/trouver-un-logement-etudiant/ville/${city.slug}`,
+    lastModified,
+    changeFrequency: 'weekly',
+    priority: 0.8,
+  }))
+  const { count, page_size } = await getAccommodations({})
+  const nbPages = Math.ceil(count / page_size)
+
+  const accommodationPagePromises = Array.from({ length: nbPages }, (_, i) => getAccommodations({ page: String(i + 1) }))
+
+  const allAccommodationPages = await Promise.all(accommodationPagePromises)
+  const allFeatures = allAccommodationPages.flatMap((page) => page.results.features)
+
+  const accommodations: MetadataRoute.Sitemap = allFeatures.map((feature) => ({
+    url: `${baseUrl}/trouver-un-logement-etudiant/ville/${feature.properties.city.replace(' ', '-').toLowerCase()}/${feature.properties.slug}`,
     lastModified,
     changeFrequency: 'weekly',
     priority: 0.8,
@@ -38,14 +54,14 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-    ...prepareStudentLifeMap,
+    ...prepareStudentLifeMetadata,
     {
       url: `${baseUrl}/trouver-un-logement-etudiant`,
       lastModified,
       changeFrequency: 'weekly',
       priority: 0.5,
     },
-    ...findStudentAccommodationMap,
+    ...findStudentAccommodationMetadata,
     {
       url: `${baseUrl}/faq`,
       lastModified,
@@ -82,5 +98,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: 'monthly',
       priority: 0.1,
     },
+    ...accommodations,
   ]
 }
