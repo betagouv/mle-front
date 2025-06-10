47e5220fefb6f497b59c454ae93fe0415770ba48
Kevin GALLET <kevingallet@gmail.com>
2025-06-10 09:23:58 +0200

    feat: add matomo

---
+++ src/app/matomo.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import init from '@socialgouv/matomo-next'
+import { useEffect } from 'react'
+
+export default function Matomo() {
+  useEffect(() => {
+    if (process.env.APP_ENV === 'production') {
+      init({
+        url: process.env.NEXT_PUBLIC_BASE_URL || '',
+        siteId: process.env.NEXT_PUBLIC_MATOMO_SITE_ID || '',
+        disableCookies: true,
+      })
+    }
+  }, [])
+
+  return null
+}
