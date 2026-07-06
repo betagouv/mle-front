e44ba5b419da961fd5a030020a450587237b8f4c
Kevin GALLET <kevingallet@gmail.com>
2026-07-06 10:59:55 +0200

    fix: hide student news message

--- src/app/(authenticated)/(espace-etudiant)/mon-espace/tableau-de-bord/page.tsx
+++ src/app/(authenticated)/(espace-etudiant)/mon-espace/tableau-de-bord/page.tsx
@@ -1,6 +1,5 @@
 import { notFound } from 'next/navigation'
 import { StudentMaximizeChances } from '~/components/student-space/dashboard/student-maximize-chances'
-import { StudentNews } from '~/components/student-space/dashboard/student-news'
 import { StudentSummary } from '~/components/student-space/dashboard/student-summary'
 import { StudentWelcome } from '~/components/student-space/dashboard/student-welcome'
 import { getServerSession } from '~/services/better-auth'
@@ -21,7 +20,7 @@ export default async function StudentDashboardPage() {
       <div className={styles.summaryContainer}>
         <StudentSummary />
         <StudentMaximizeChances />
-        <StudentNews />
+        {/*<StudentNews />*/}
       </div>
     </>
   )
